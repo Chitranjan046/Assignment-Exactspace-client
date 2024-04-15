a112b0d71dc271b2a86c890dd08e81f77cd55c01
Chitranjan  Kumar Patel <chitranjankumarpatel8@gmail.com>
2024-04-16 03:18:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+import {UserContextProvider} from "./UserContext";
+import Routes from "./Routes";
+
+function App() {
+  axios.defaults.baseURL = 'https://chat-app-cwd1.onrender.com/';
+  axios.defaults.withCredentials = true;
+  return (
+    <UserContextProvider>
+      <Routes />
+    </UserContextProvider>
+  )
+}
+
+export default App
